c0fbc91771289891bd852af6aa3e0b5d0b26352d
Marlon Barreto <mbarretot@hotmail.com>
2026-02-12 15:54:14 -0500

    chore: add py.typed marker and __all__ exports

--- src/dataset_curator/__init__.py
+++ src/dataset_curator/__init__.py
@@ -1,3 +1,20 @@
 """Dataset Curator."""
 
+__all__ = [
+    "ChatMessage",
+    "CurationPipeline",
+    "DataCleaner",
+    "DatasetStats",
+    "Deduplicator",
+    "FormatConverter",
+    "RawExample",
+    "TrainingExample",
+]
+
+from .cleaner import DataCleaner
+from .converter import FormatConverter
+from .deduplicator import Deduplicator
+from .models import ChatMessage, DatasetStats, RawExample, TrainingExample
+from .pipeline import CurationPipeline
+
 __version__ = "0.1.0"
